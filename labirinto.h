5952012c98140872de422fbcf5cb6e7e99c39187
João Pedro <joaopedro.carvalho@sou.unifal-mg.edu.br>
2024-09-19 13:07:22 -0300

    Primeiro commit

---
+++ labirinto.h
@@ -0,0 +1,51 @@
+/*
+ * Projeto | AEDS II - Prática
+ *
+ * Descrição: Projeto para encontrar as coordenadas do caminho entre a entrada e a saída de um labirinto em formato .txt.
+ *
+ * Nomes: Guilherme Oliveira Araújo    2024.1.08.011
+ *        João Antonio Lassister Melo    2024.1.08.027
+ *        João Pedro Carvalho Ferreira    2024.1.08.030
+ */
+
+#ifndef LABIRINTO_H
+#define LABIRINTO_H
+
+// estrutura dos nós da matriz labirinto
+typedef struct{
+    // campo que guarda o caractere do labirinto na posição
+    char chave;
+    // campo que guarda a quantidade de caminhos possíveis de serem tomados naquela posição
+    int caminhos;
+}no_labirinto;
+// estrutura de matriz 12x12 de nós com os campos "chave" e "caminhos"
+typedef no_labirinto TMatriz[12][12];
+
+// estrutura dos nós da pilha
+typedef struct no_ no_pilha;
+ struct no_{
+    // campo que guarda a coordenada "i"
+    int linha;
+    // campo que guarda a coordenada "j"
+    int coluna;
+    // ponteiro que aponta para o próximo nó
+    no_pilha *prox;
+};
+
+void insere_pilha(no_pilha **topo, no_pilha *novo_no);
+
+void remove_pilha(no_pilha **topo);
+
+void mostra_pilha(no_pilha *topo);
+
+void preenche_bordas(TMatriz labirinto);
+
+void preenche_matriz(TMatriz labirinto, FILE *file, no_pilha *saida);
+
+void move_no_labirinto(TMatriz labirinto, int *i, int *j);
+
+void conta_caminhos(TMatriz labirinto, int i, int j);
+
+void retornar_a_posicao(TMatriz labirinto, no_pilha **topo, int *i, int *j);
+
+#endif
---
+++ main.c
@@ -0,0 +1,89 @@
+/*
+ * Projeto | AEDS II - Prática
+ *
+ * Descrição: Projeto para encontrar as coordenadas do caminho entre a entrada e a saída de um labirinto em formato .txt.
+ *
+ * Nomes: Guilherme Oliveira Araújo    2024.1.08.011
+ *        João Antonio Lassister Melo    2024.1.08.027
+ *        João Pedro Carvalho Ferreira    2024.1.08.030
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "labirinto.h"
+#include <string.h>
+
+/*
+ * Função principal.
+ */
+int main(int argc, char *argv[]){
+    // matriz que receberá o arquivo .txt
+    TMatriz labirinto;
+    // ponteiro que apontará para o primeiro nó da pilha
+    no_pilha *topo = NULL;
+    // alocação do nó que receberá as coordenadas da saída do labirinto
+    no_pilha *saida = malloc(sizeof(no_pilha));
+
+    // verificação da quantidade de argumentos
+    if(argc != 2){
+        puts("Uso: ./hello <num>");
+        return 1;
+    }
+
+    char caminho_total[30];
+    // caminho até a pasta com os labirintos
+    strcpy(caminho_total, "./labirintos/labirinto");
+    // seleção de qual labirinto será escolhido como entrada a patir do argumento
+    strcat(caminho_total, argv[1]);
+    strcat(caminho_total, ".txt");
+
+    // atribuição de "X" às bordas da matriz
+    preenche_bordas(labirinto);
+
+    // abertura do arquivo labirindo.txt
+    FILE *file = fopen(caminho_total, "r");
+
+    // preenchimento da matriz com os caracteres do arquivo .txt
+    preenche_matriz(labirinto, file, saida);
+
+    // fechameto do arquivo labirinto.txt
+    fclose(file);
+
+    // inserção do nó com as coordenadas da saída na pilha
+    insere_pilha(&topo, saida);
+
+    // índices para percorrer a matriz
+    int i, j;
+    // for com inicialização dos índices com as coordenadas da saída do labirinto e condição de permanência enquanto a entrada não for encontrada
+    for (i = saida->linha, j = saida->coluna; labirinto[i][j].chave != 'E'; ){
+
+        // atribuição de "X" à posição atual
+        labirinto[i][j].chave = 'X';
+
+        // movendo-se para onde não tenha parede
+        move_no_labirinto(labirinto, &i, &j);
+
+        // alocação de um novo nó para as novas coordenadas
+        no_pilha *no_insere = malloc(sizeof(no_pilha));
+        no_insere->linha = i;
+        no_insere->coluna = j;
+        // inserção do nó com as novas coordenadas na pilha
+        insere_pilha(&topo, no_insere);
+
+        // contabilização da quatidade de caminhos possíveis de serem tomados a partir daquela posição
+        conta_caminhos(labirinto, i, j);
+
+        // verificação para saber se chegou a um beco
+        if (labirinto[i][j].caminhos == 0 && labirinto[i][j].chave != 'E'){
+            // retorno a uma posição que tenha mais de 1 caminho possível de ser tomado
+            retornar_a_posicao(labirinto, &topo, &i, &j);
+            // diminuição da quatidade de caminhos possíveis de serem tamados naquela posição
+            labirinto[i][j].caminhos -= 1;
+        }
+    }
+
+    // impressão das coordenadas na tela
+    mostra_pilha(topo);
+
+    return 0;
+}
